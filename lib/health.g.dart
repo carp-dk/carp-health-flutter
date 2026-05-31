fa5dd3ea02d06a0253715c7e18dab8a6a191d2b1
Zayin Krige <zkrige@gmail.com>
2026-05-31 17:22:35 +0200

    Add per-workout flights climbed and swimming stroke count

--- lib/health.g.dart
+++ lib/health.g.dart
@@ -291,6 +291,9 @@ WorkoutHealthValue _$WorkoutHealthValueFromJson(Map<String, dynamic> json) =>
         _$HealthDataUnitEnumMap,
         json['totalStepsUnit'],
       ),
+      totalFlightsClimbed: (json['totalFlightsClimbed'] as num?)?.toInt(),
+      totalSwimmingStrokeCount: (json['totalSwimmingStrokeCount'] as num?)
+          ?.toInt(),
     )..$type = json['__type'] as String?;
 
 Map<String, dynamic> _$WorkoutHealthValueToJson(WorkoutHealthValue instance) =>
@@ -305,6 +308,8 @@ Map<String, dynamic> _$WorkoutHealthValueToJson(WorkoutHealthValue instance) =>
       'totalDistanceUnit': ?_$HealthDataUnitEnumMap[instance.totalDistanceUnit],
       'totalSteps': ?instance.totalSteps,
       'totalStepsUnit': ?_$HealthDataUnitEnumMap[instance.totalStepsUnit],
+      'totalFlightsClimbed': ?instance.totalFlightsClimbed,
+      'totalSwimmingStrokeCount': ?instance.totalSwimmingStrokeCount,
     };
 
 const _$HealthWorkoutActivityTypeEnumMap = {
